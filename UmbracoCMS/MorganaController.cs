5217e7ee71429fdb4fc45bc0b49e78328afba4aa
cabuin <camila.abuin@globant.com>
2025-06-19 19:45:42 -0300

    Add isOk endpoint

---
+++ UmbracoCMS/MorganaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Controllers;
+using Umbraco.Cms.Api.Management.Routing;
+
+namespace UmbracoCMS;
+
+[VersionedApiBackOfficeRoute("is-ok")]
+[ApiExplorerSettings(GroupName = "Morgana")]
+public class MorganaController : ManagementApiControllerBase
+{
+    [HttpGet]
+    public IActionResult CheckIsOk([FromQuery] bool isOk)
+    {
+        return isOk ? Ok("It's OK") : BadRequest("It is not OK");
+    }
+}
